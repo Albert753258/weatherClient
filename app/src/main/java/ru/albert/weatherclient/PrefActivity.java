913d262d6f56df9156818e4e1186a3472ab3f04f
Albert <albert.nasyrov2016@gmail.com>
2020-05-09 12:36:55 +0300

    Теперь для выбора не надо нажимать кнопку сохранить, после выбора настройки автоматически закрываются и отображается температура

--- app/src/main/java/ru/albert/weatherclient/PrefActivity.java
+++ app/src/main/java/ru/albert/weatherclient/PrefActivity.java
@@ -1,13 +1,12 @@
 package ru.albert.weatherclient;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -20,12 +19,16 @@ public class PrefActivity extends AppCompatActivity {
         String[] cityes = {"Москва", "Санкт-Петербург", "Новосибирск", "Екатеринбург", "Нижний Новгород", "Казань", "Челябинск", "Омск", "Самара", "Ростов-на-Дону"};
         AutoCompleteTextView autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
         List<String> cityesList = Arrays.asList(cityes);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                this, android.R.layout.simple_dropdown_item_1line, cityesList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, cityesList);
         autoCompleteTextView.setAdapter(adapter);
-    }
-    public void saveButtonClicked(View view) {
-        AutoCompleteTextView cityView = findViewById(R.id.autoCompleteTextView);
-        weatherGetter.getWeather(MainActivity.textView, citySearch.getURL(cityView.getText().toString()));
+        Activity activity = this;
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View arg1, int position, long arg3) {
+                Object item = parent.getItemAtPosition(position);
+                weatherGetter.getWeather(MainActivity.textView, citySearch.getURL(item.toString()));
+                activity.finish();
+            }
+        });
     }
 }
